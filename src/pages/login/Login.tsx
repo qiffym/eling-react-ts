353b24673607a725e8e1e2ed84184d8a3854c094
Qiff Ya Muhammad <qiww.dev@gmail.com>
2022-10-09 20:37:43 +0700

    new login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -33,76 +33,133 @@ const Login = () => {
 
   return (
     <>
-      {/* <Helmet>
+      <Helmet>
         <title>Login</title>
-      </Helmet> */}
-      <div className="flex w-screen h-screen bg-yellow-300">
-        <div className="bg-slate-200 m-auto w-full max-w-sm flex flex-col justify-center content-center items-center border-4 border-slate-400 rounded-xl shadow-2xl px-7 py-14">
-          <img src={logosmk} alt="logo_smk" className="mb-4" />
-          <h2 className="text-4xl font-bold text-teal-600">Masuk</h2>
-          <h3 className="text-lg font-semibold mb-2">
-            E-Learning SMK Negeri 3 Malang
-          </h3>
-          <div className="border-t-2 w-1/2 border-teal-600 inline-block mb-4"></div>
-          <form onSubmit={e => authLogin(e, input.username, input.password)}>
-            {/* Username */}
-            <label htmlFor="username" className="text-slate-600">
-              <FaUserAlt className="text-md inline mr-1 pb-1" />
-              Username
-            </label>
-            <input
-              type="text"
-              placeholder="username / email"
-              name="username"
-              id="username"
-              className="input input-bordered w-full focus:outline-none focus:ring ring-teal-600 mb-3"
-              value={input.username}
-              onChange={handleChange}
-            />
+      </Helmet>
+      <section id="login" className="bg-yellow-300 min-h-screen">
+        <div className="container mx-auto pt-16">
+          <div className="grid grid-cols-none md:grid-cols-3 px-10 text-center max-h-screen xl:w-full xl:px-60">
+            {/* Logo Sekolah */}
+            <div className="hidden md:block bg-white rounded-tl-2xl rounded-bl-2xl shadow-lg">
+              <img
+                src={logosmk}
+                alt="logo_smk"
+                width={200}
+                className="mx-auto my-10"
+              />
+            </div>
 
-            {/* Password */}
-            <label htmlFor="password" className="text-slate-600">
-              <FaLock className="text-md inline mr-1 pb-1" />
-              Password
-            </label>
-            <input
-              type="password"
-              placeholder="*********"
-              name="password"
-              id="password"
-              autoComplete="on"
-              className="input input-bordered w-full focus:outline-none focus:ring ring-teal-600 mb-3"
-              value={input.password}
-              onChange={handleChange}
-            />
-            <button
-              disabled={disable}
-              className="btn w-full bg-teal-600 border-slate-400">
-              {isLoading ? <LoadingButton /> : 'Login'}
-            </button>
-          </form>
-          {/* Footer */}
-          <div className="border-t-2 w-72 border-slate-400 inline-block mb-3 mt-4"></div>
-          <div className="inline-block">
-            <h3 className="italic font-semibold text-slate-500 hover:text-slate-700 hover:font-bold">
-              #SMKBisa #SMKHebat
-            </h3>
-          </div>
-          <div className="mt-5 text-center text-slate-600 text-xs">
-            <p>&copy; {currentYear} E-Learning SMK Negeri 3 Malang</p>
-            <p>
-              Design with ❤️ by.{' '}
-              <a
-                href="http://qiffyamuhammad.my.id"
-                rel="noopener noreferrer"
-                target="_blank"
-                className="text-teal-600">
-                Qiff Ya Muhammad
-              </a>
-            </p>
+            {/* Login Form */}
+            <div className="md:col-span-2 shadow-xl">
+              <div className="bg-slate-200 rounded-2xl sm:rounded-none sm:rounded-tr-2xl sm:rounded-br-2xl">
+                <div className="text-left text-sm font-bold pl-5 pt-2">
+                  <span className="text-teal-600">Smeka</span>Negama
+                </div>
+                <div className="py-10">
+                  <div className="md:hidden">
+                    <img
+                      src={logosmk}
+                      alt="logo_smk"
+                      className="mx-auto mb-3"
+                    />
+                  </div>
+                  <h2 className="text-4xl font-bold text-teal-600">Masuk</h2>
+                  <h3 className="text-lg font-semibold">
+                    e-Learning SMK Negeri 3 Malang
+                  </h3>
+                  <div className="border-t-2 w-10 border-teal-600 inline-block mb-2"></div>
+                  <form
+                    onSubmit={e =>
+                      authLogin(e, input.username, input.password)
+                    }>
+                    <div className="flex flex-col items-center">
+                      {/* Username */}
+                      <div className="bg-slate-100 w-72 p-2 flex items-center rounded-xl mb-2">
+                        <FaUserAlt className="mr-3 text-gray-400 text-sm" />
+                        <input
+                          type="text"
+                          placeholder="username / email"
+                          name="username"
+                          id="username"
+                          className="outline-none text-sm bg-slate-100"
+                          value={input.username}
+                          onChange={handleChange}
+                        />
+                      </div>
+
+                      {/* Password */}
+                      <div className="bg-slate-100 w-72 p-2 flex items-center rounded-xl">
+                        <FaLock className="mr-3 text-gray-400 text-sm" />
+                        <input
+                          type="password"
+                          placeholder="*********"
+                          name="password"
+                          id="password"
+                          autoComplete="on"
+                          className="outline-none text-sm bg-slate-100"
+                          value={input.password}
+                          onChange={handleChange}
+                        />
+                      </div>
+
+                      {/* Remember Me & Forget Password */}
+                      <div className="w-72 flex justify-between mb-5 mt-1">
+                        <label
+                          htmlFor="remember"
+                          className="flex items-center text-xs text-slate-500 hover:text-slate-700">
+                          <input
+                            type="checkbox"
+                            name="remember"
+                            id="remember"
+                            className="mr-1"
+                            // onChange={handleChange}
+                          />{' '}
+                          Ingat saya
+                        </label>
+                        <a
+                          href="#!"
+                          className="text-xs text-slate-500 hover:text-slate-700">
+                          Lupa password?
+                        </a>
+                      </div>
+
+                      {/* Button Submit */}
+                      <button
+                        disabled={disable}
+                        type="submit"
+                        className="btn w-72 bg-teal-600 border-slate-400">
+                        {isLoading ? <LoadingButton /> : 'Masuk'}
+                      </button>
+                    </div>
+                  </form>
+
+                  {/* Footer */}
+                  <div className="border-t-2 w-72 border-slate-400 inline-block mb-2"></div>
+                  <br />
+                  <div className="inline-block">
+                    <h3 className="italic font-semibold text-slate-500 hover:text-slate-700 hover:font-bold">
+                      #SMKBisa #SMKHebat
+                    </h3>
+                  </div>
+                  <div className="mt-5 text-center text-slate-500 text-xs mr-5">
+                    <p>&copy; {currentYear} E-Learning SMK Negeri 3 Malang</p>
+                    <p>
+                      Design with ❤️ by.{' '}
+                      <a
+                        href="http://qiffyamuhammad.my.id"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="text-teal-600">
+                        Qiff Ya Muhammad
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
