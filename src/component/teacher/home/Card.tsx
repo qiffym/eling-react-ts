62182ced28c0f286d6bcf25f230317a42b274e41
Muhammad Vikri <muhammadvikrii99@gmail.com>
2022-10-09 21:25:45 +0700

    Add API on Detail Class

--- src/component/teacher/home/Card.tsx
+++ src/component/teacher/home/Card.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FC } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { Classes } from '../../../types/class-type';
+import { useNavigate } from 'react-router-dom';
+import { ClassesType } from '../../../types/class-type';
 
 type ListClass = {
-  classes: Classes[] | undefined;
+  classes: ClassesType[] | undefined;
 };
 
 const CardClass: FC<ListClass> = ({ classes }) => {
@@ -12,65 +12,51 @@ const CardClass: FC<ListClass> = ({ classes }) => {
 
   return (
     <>
-      <section id="my-class-teacher">
-        <div className="flex justify-between items-center">
-          <div>
-            <h2 className="text-2xl font-medium">My Class</h2>
-          </div>
-          <div>
-            <input
-              type="search"
-              placeholder="Search"
-              className="input input-bordered w-96 max-w-xs rounded-full"
-            />
-          </div>
-        </div>
-        <hr className="border-t border-black my-4" />
-
-        {/* Card Class */}
-        <div className="grid grid-cols-3 gap-5">
-          {classes?.map(item => (
-            <div key={item.id} className="card w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="font-bold text-xl">{item.rombel_name}</h2>
-                <h2
-                  onClick={() =>
-                    navigate(`/online-class/${item.id}`, { replace: true })
-                  }
-                  className="card-title cursor-pointer">
-                  {item.name}
-                </h2>
-                <p className="font-medium text-sm text-slate-600">
-                  <span>Senin</span> | <span>07.30 - 09.00 WIB</span>
-                </p>
-                {/* <p>{item.description}</p> */}
-                <hr className="border-t-2" />
-                <div className="card-actions flex flex-row justify-between items-center">
-                  <div className="flex items-center space-x-2">
-                    <div className="avatar">
-                      <div className="mask mask-circle w-7 h-7">
-                        <img
-                          className="w-10 rounded-full"
-                          src={item.teacher_avatar}
-                          alt="teacher"
-                        />
-                      </div>
+      {/* Card Class */}
+      <div className="grid grid-cols-3 gap-5">
+        {classes?.map(item => (
+          <div key={item.id} className="card w-[22rem] bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="font-bold text-xl">{item.rombel_name}</h2>
+              <h2
+                onClick={() =>
+                  navigate(`/online-class/${item.id}`, { replace: true })
+                }
+                className="card-title cursor-pointer">
+                {item.name}
+              </h2>
+              <p className="font-medium text-sm text-slate-600">
+                <span>Senin</span> | <span>07.30 - 09.00 WIB</span>
+              </p>
+              {/* <p>{item.description}</p> */}
+              <hr className="border-t-2" />
+              <div className="card-actions flex flex-row justify-between items-center">
+                <div className="flex items-center space-x-2">
+                  <div className="avatar">
+                    <div className="mask mask-circle w-7 h-7">
+                      <img
+                        className="w-10 rounded-full"
+                        src={item.teacher_avatar}
+                        alt="teacher"
+                      />
                     </div>
-                    <p className="font-semibold text-sm">{item.teacher_name}</p>
                   </div>
-                  <button
-                    onClick={() =>
-                      navigate(`/online-class/${item.id}`, { replace: true })
-                    }
-                    className="btn btn-ghost">
-                    View
-                  </button>
+                  <p className="font-semibold text-sm">{item.teacher_name}</p>
                 </div>
+                <button
+                  onClick={() =>
+                    navigate(`/online-class/${item.id}`, {
+                      state: item,
+                    })
+                  }
+                  className="btn btn-ghost">
+                  View
+                </button>
               </div>
             </div>
-          ))}
-        </div>
-      </section>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
--- src/pages/teacher/dashboard/DashboardTeacher.tsx
+++ src/pages/teacher/dashboard/DashboardTeacher.tsx
@@ -1,14 +1,11 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { HiPlus } from 'react-icons/hi';
 import Loading from '../../../component/loading/Loading';
 import CreateClassModal from '../../../component/modal/CreateClassModal';
-import Modal from '../../../component/modal/Modal';
 import Header from '../../../component/header/Header';
 import Footer from '../../../component/layout/Footer';
-
 import CardClass from '../../../component/teacher/home/Card';
-import { MyContext } from '../../../context/context';
 import { useClasses } from '../../../hooks/useClasses';
 
 const DashboardTeacher = () => {
@@ -21,36 +18,50 @@ const DashboardTeacher = () => {
         <title>Dashboard</title>
       </Helmet>
       <Header>Dashboard</Header>
-      <button
-        onClick={() => setOpenModal(true)}
-        className="btn flex w-14 h-14 btn-primary rounded-full fixed z-10 right-5 bottom-5 items-center content-center justify-center drop-shadow-md">
-        <HiPlus className="text-xl" />
-      </button>
-
-      {isLoading ? (
-        <Loading />
-      ) : classList?.length ? (
-        <CardClass classes={classList} />
-      ) : (
-        <div className="flex h-[40rem]">
-          <div className="m-auto flex flex-col">
-            <button
-              onClick={() => setOpenModal(true)}
-              className="btn btn-ghost font-extrabold text-5xl">
-              Tambah Kelas
-            </button>
+      <section>
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-2xl font-medium">My Class</h2>
+          </div>
+          <div>
+            <input
+              type="search"
+              placeholder="Search"
+              className="input input-bordered w-96 max-w-xs rounded-full"
+            />
           </div>
         </div>
-      )}
-      {openModal ? (
-        <CreateClassModal
-          actionSave={() => {
-            setOpenModal(false);
-          }}
-          modalAction={() => setOpenModal(false)}
-        />
-      ) : null}
+        <hr className="border-t border-gray-300 my-4" />
+        <button
+          onClick={() => setOpenModal(true)}
+          className="btn flex w-14 h-14 btn-primary rounded-full fixed z-10 right-5 bottom-5 items-center content-center justify-center drop-shadow-md">
+          <HiPlus className="text-xl" />
+        </button>
 
+        {isLoading ? (
+          <Loading />
+        ) : classList?.length ? (
+          <CardClass classes={classList} />
+        ) : (
+          <div className="flex h-[40rem]">
+            <div className="m-auto flex flex-col">
+              <button
+                onClick={() => setOpenModal(true)}
+                className="btn btn-ghost font-extrabold text-5xl">
+                Tambah Kelas
+              </button>
+            </div>
+          </div>
+        )}
+        {openModal ? (
+          <CreateClassModal
+            actionSave={() => {
+              setOpenModal(false);
+            }}
+            modalAction={() => setOpenModal(false)}
+          />
+        ) : null}
+      </section>
       <Footer />
     </>
   );
