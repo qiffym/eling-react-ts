d3b0416d8edf88fbeb457ba107555d0d85ddcc0f
Muhammad Vikri <muhammadvikrii99@gmail.com>
2022-10-26 21:31:02 +0700

    New Design Responsive

--- src/component/layout/index.tsx
+++ src/component/layout/index.tsx
@@ -5,7 +5,14 @@ import Sidebar from './Sidebar';
 const LayoutIndex = () => (
   <div className="flex min-h-screen">
     <Sidebar />
-    <div id="bodyDash" className="flex-1 bg-[#f5f6fa] h-full ml-64">
+    <div
+      id="bodyDash"
+      className="hidden sm:block flex-1 bg-[#f5f6fa] h-full ml-64">
+      <Outlet />
+    </div>
+    <div
+      id="mobile-body"
+      className="sm:hidden block flex-1 bg-[#f5f6fa] h-full overflow-auto">
       <Outlet />
     </div>
   </div>
