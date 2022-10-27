c9fb1588c0188b86db9e76c0f88457d96d390fa9
Muhammad Vikri <muhammadvikrii99@gmail.com>
2022-10-27 12:02:11 +0700

    No Connection Detector

--- src/component/layout/HeaderIndex.tsx
+++ src/component/layout/HeaderIndex.tsx
@@ -28,8 +28,22 @@ const HeaderIndex = () => {
           </button>
         </nav> */}
       {isOnline ? null : (
-        <div className="bg-red-500 px-3 font-medium fixed z-50 w-screen text-white transition-all duration-100">
-          No Connection
+        <div className="alert alert-error rounded-none shadow-lg transition-all duration-100">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>Error! No Connection.</span>
+          </div>
         </div>
       )}
       <Outlet />
